a2101c7a6528e219444bc6a727ee8cc1b081b929
Éderson Cássio <ederson_cassio@yahoo.com.br>
2019-06-17 15:50:31 -0300

    Refactorings para testabilidade

--- app/assets/javascripts/materials.js
+++ app/assets/javascripts/materials.js
@@ -1,3 +1,6 @@
+var URL_UP = '/voto-up';
+var URL_DOWN = '/voto-down';
+
 function atualizaPorAjax(combo_disciplina, id_curso) {
     $.get(
         '/cursos/' + id_curso + '/disciplinas.json',
@@ -13,41 +16,48 @@ function atualizaPorAjax(combo_disciplina, id_curso) {
     );
 }
 
+function interfaceMaterial(id) {
+    return {
+        pontos: $('.voto_material[data-pontos-id=' + id + ']'),
+        meu_voto: $('.meu_voto[data-pontos-id=' + id + ']'),
+        up: $('a.up[data-id=' + id + ']'),
+        down: $('a.down[data-id=' + id + ']')
+    };
+}
+
+function atualiza(interf, data) {
+    console.log(data);
+    interf.pontos.text(data.pontos + (Math.abs(data.pontos) > 1 ? ' pontos' : ' ponto'));
+    interf.meu_voto.text(data.voto == +1 ? 'Gostei' : (data.voto == -1 ? 'Não gostei' : ''));
+    interf.up.html('<img src="/assets/' + (data.voto == +1 ? 'up-ativo' : 'up-inativo') + '.png">');
+    interf.down.html('<img src="/assets/' + (data.voto == -1 ? 'down-ativo' : 'down-inativo') + '.png">');
+}
+
+function cliqueLike(url, id, interf) {
+    $.post(
+        url, 
+        {id: id},
+        function(data) {
+            atualiza(interf, data);
+        }
+    );
+}
 
 $(document).ready(function() {
 
     $(document).on('click', 'a.up', function() {
         var id = $(this).data('id');
-        $.post(
-            '/voto-up', 
-            {id: id},
-            function(data) {
-                atualiza(id, data.pontos, data.voto);
-            }
-        );
-    });
-
-    $(document).on('click', 'a.down', function() {
-        var id = $(this).data('id');
-        $.post(
-            '/voto-down', 
-            {id: id},
-            function(data) {
-                atualiza(id, data.pontos, data.voto);
-            }
-        );
+        cliqueLike(URL_UP, id, interfaceMaterial(id));
     });
     
+    $(document).on('click', 'a.down', function() {
+        var id = $(this).data('id');
+        cliqueLike(URL_DOWN, id, interfaceMaterial(id));
+    });
+
     $(document).on('change', 'p.selecao_curso select', function() {
         var id_curso = parseInt($(this).val());
         atualizaPorAjax($('#material_disciplina_id'), id_curso);
     });
 
 });
-
-function atualiza(id, pontos, meu_voto) {
-    $('.voto_material[data-pontos-id=' + id + ']').text(pontos + (Math.abs(pontos) > 1 ? ' pontos' : ' ponto'));
-    $('.meu_voto[data-pontos-id=' + id + ']').text(meu_voto == +1 ? 'Gostei' : (meu_voto == -1 ? 'Não gostei' : ''));
-    $('a.up[data-id=' + id + ']').html('<img src="/assets/' + (meu_voto == +1 ? 'up-ativo' : 'up-inativo') + '.png">');
-    $('a.down[data-id=' + id + ']').html('<img src="/assets/' + (meu_voto == -1 ? 'down-ativo' : 'down-inativo') + '.png">');
-}
