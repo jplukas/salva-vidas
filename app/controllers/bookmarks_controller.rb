35a35329fecedd2846856cdb9c994de695298d6a
jplukas <jplukasavicus@gmail.com>
2019-06-25 11:54:37 -0300

    Finaliza funcionalidade basica de bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,19 @@
 class BookmarksController < ApplicationController
     def create
         @material = Material.find(params[:material_id])
-        byebug
         current_user.bookmark(@material) unless current_user.bookmarked?(@material) 
+        respond_to do |format|
+            format.html{redirect_to @material}
+            format.js
+        end
+    end
+
+    def destroy
+        @material = Bookmark.find(params[:id]).material
+        current_user.un_bookmark(@material) if current_user.bookmarked?(@material) 
+        respond_to do |format|
+            format.html{redirect_to @material}
+            format.js
+        end
     end
 end
